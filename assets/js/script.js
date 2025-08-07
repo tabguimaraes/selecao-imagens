401fbe113aeb30dcb193fe2488e8864d8d0b43ca
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-08-07 10:05:26 -0300

    add funcao de captura do valor do input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const btnImagem = document.querySelector("#btn-imagem");
+const slider = document.querySelectorAll(".slider");
 const nomeFoto = document.querySelector("#nome-foto");
 
-console.log(nomeFoto);
+btnImagem.addEventListener("click", function (evento) {
+  slider.forEach((element) => {
+    element.classList.toggle("slider");
+
+    //Verificar funcionamento
+    // element.setAttribute("style", "background-image: url(`./assets/img/cidade.jpg`)");
+    // element.setAttribute("style", `background-size: cover`);
+  });
+});
+
+nomeFoto.addEventListener("change", function (evento) {
+  console.log(evento.target.value);
+});

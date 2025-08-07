f729a13883bb1906118b7d5fc5c2b00d6443eed1
Tiago Guimaraes <tab.guimaraes@gmail.com>
2025-08-07 17:24:30 -0300

    Testes OK. Versão de lançamento 1.0.8.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,26 @@
 "use strict";
 
-const classes = ["img1", "img2", "img3", "img4", "img5"];
-const locais = ["Cidade", "Deck", "Floresta", "Lago", "Montanha"];
+let entrada = "";
+const classesIMG = ["img1", "img2", "img3", "img4", "img5"];
+const locaisIMG = ["Cidade", "Deck", "Floresta", "Lago", "Montanha"];
 const btnImagem = document.querySelector("#btnImagem");
 const sliderEffect = document.querySelectorAll(".sliderEffect");
 const fadeEffect = document.querySelector(".fadeEffect");
 const textInputField = document.querySelector("#textInputField");
 const imgContainer = document.querySelectorAll(".imgContainer");
-let entrada = "";
 const tituloCidade = document.querySelector("#tituloCidade");
 
 btnImagem.addEventListener("click", function (evento) {
   evento.preventDefault();
+  textInputField.value = "";
   textInputField.setAttribute("placeholder", "");
   fadeEffect.classList.add("fadeEffect");
+
   imgContainer.forEach((element) => {
+    tituloCidade.innerText = "";
+    element.classList.remove(...classesIMG);
+
     element.classList.add("sliderEffect");
-    element.classList.add("imgContainer");
   });
 });
 
@@ -28,10 +32,10 @@ textInputField.addEventListener("change", function (evento) {
     case "1":
     case "cidade":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "sliderEffect");
+        element.classList.remove(...classesIMG, "sliderEffect");
         fadeEffect.classList.remove("fadeEffect");
-        element.classList.add(classes[0]);
-        tituloCidade.innerHTML = locais[0];
+        element.classList.add(classesIMG[0]);
+        tituloCidade.innerHTML = locaisIMG[0];
       });
 
       break;
@@ -39,47 +43,48 @@ textInputField.addEventListener("change", function (evento) {
     case "2":
     case "deck":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "sliderEffect");
+        element.classList.remove(...classesIMG, "sliderEffect");
         fadeEffect.classList.remove("fadeEffect");
-        element.classList.add(classes[1]);
-        tituloCidade.innerHTML = locais[1];
+        element.classList.add(classesIMG[1]);
+        tituloCidade.innerHTML = locaisIMG[1];
       });
       break;
     case "03":
     case "3":
     case "floresta":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "sliderEffect");
+        element.classList.remove(...classesIMG, "sliderEffect");
         fadeEffect.classList.remove("fadeEffect");
-        element.classList.add(classes[2]);
-        tituloCidade.innerHTML = locais[2];
+        element.classList.add(classesIMG[2]);
+        tituloCidade.innerHTML = locaisIMG[2];
       });
       break;
     case "04":
     case "4":
     case "lago":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "sliderEffect");
+        element.classList.remove(...classesIMG, "sliderEffect");
         fadeEffect.classList.remove("fadeEffect");
-        element.classList.add(classes[3]);
-        tituloCidade.innerHTML = locais[3];
+        element.classList.add(classesIMG[3]);
+        tituloCidade.innerHTML = locaisIMG[3];
       });
       break;
     case "05":
     case "5":
     case "montanha":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "sliderEffect");
+        element.classList.remove(...classesIMG, "sliderEffect");
         fadeEffect.classList.remove("fadeEffect");
-        element.classList.add(classes[4]);
-        tituloCidade.innerHTML = locais[4];
+        element.classList.add(classesIMG[4]);
+        tituloCidade.innerHTML = locaisIMG[4];
       });
       break;
 
     default:
       textInputField.value = "";
       textInputField.setAttribute("placeholder", "Não encontrado");
-
+      tituloCidade.innerHTML = "";
       break;
   }
+  textInputField.value = "";
 });

7ce78fa97f2c52aa6445e67db191d59fcfe98786
Tiago Guimaraes <tab.guimaraes@gmail.com>
2025-08-07 15:37:20 -0300

    logica parcialmente pronta. necessario testes e refatoracao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,76 @@
 "use strict";
 
+const classes = ["img1", "img2", "img3", "img4", "img5"];
 const btnImagem = document.querySelector("#btn-imagem");
 const slider = document.querySelectorAll(".slider");
-const nomeFoto = document.querySelector("#nome-foto");
+const fade = document.querySelector(".fade");
+const inputFoto = document.querySelector("#inputFoto");
+const imgContainer = document.querySelectorAll(".imgContainer");
+let entrada = "";
 
 btnImagem.addEventListener("click", function (evento) {
-  slider.forEach((element) => {
-    element.classList.toggle("slider");
-
-    //Verificar funcionamento
-    // element.setAttribute("style", "background-image: url(`./assets/img/cidade.jpg`)");
-    // element.setAttribute("style", `background-size: cover`);
+  evento.preventDefault();
+  fade.classList.add("fade");
+  imgContainer.forEach((element) => {
+    element.classList.add("slider");
+    element.classList.add("imgContainer");
   });
 });
 
-nomeFoto.addEventListener("change", function (evento) {
-  console.log(evento.target.value);
+inputFoto.addEventListener("change", function (evento) {
+  entrada = String(evento.target.value).toLowerCase();
+
+  console.log(entrada);
+
+  switch (entrada) {
+    case "01":
+    case "1":
+    case "cidade":
+      imgContainer.forEach((element) => {
+        element.classList.remove(...classes, "slider");
+        fade.classList.remove("fade");
+        element.classList.add(classes[0]);
+      });
+
+      break;
+    case "02":
+    case "2":
+    case "deck":
+      imgContainer.forEach((element) => {
+        element.classList.remove(...classes, "slider");
+        fade.classList.remove("fade");
+        element.classList.add(classes[1]);
+      });
+      break;
+    case "03":
+    case "3":
+    case "floresta":
+      imgContainer.forEach((element) => {
+        element.classList.remove(...classes, "slider");
+        fade.classList.remove("fade");
+        element.classList.add(classes[2]);
+      });
+      break;
+    case "04":
+    case "4":
+    case "lago":
+      imgContainer.forEach((element) => {
+        element.classList.remove(...classes, "slider");
+        fade.classList.remove("fade");
+        element.classList.add(classes[3]);
+      });
+      break;
+    case "05":
+    case "5":
+    case "montanha":
+      imgContainer.forEach((element) => {
+        element.classList.remove(...classes, "slider");
+        fade.classList.remove("fade");
+        element.classList.add(classes[4]);
+      });
+      break;
+
+    default:
+      break;
+  }
 });

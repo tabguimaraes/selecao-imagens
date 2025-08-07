33cc9ad3411031b7c00df8ee3ac1ae88d8223fed
Tiago Guimaraes <tab.guimaraes@gmail.com>
2025-08-07 17:00:52 -0300

    ajuste de nomes de variáveis. pendente testes na logica

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,25 @@
 
 const classes = ["img1", "img2", "img3", "img4", "img5"];
 const locais = ["Cidade", "Deck", "Floresta", "Lago", "Montanha"];
-const btnImagem = document.querySelector("#btn-imagem");
-const slider = document.querySelectorAll(".slider");
-const fade = document.querySelector(".fade");
-const inputFoto = document.querySelector("#inputFoto");
+const btnImagem = document.querySelector("#btnImagem");
+const sliderEffect = document.querySelectorAll(".sliderEffect");
+const fadeEffect = document.querySelector(".fadeEffect");
+const textInputField = document.querySelector("#textInputField");
 const imgContainer = document.querySelectorAll(".imgContainer");
 let entrada = "";
-const localP = document.querySelector("#local-p");
+const tituloCidade = document.querySelector("#tituloCidade");
 
 btnImagem.addEventListener("click", function (evento) {
   evento.preventDefault();
-  inputFoto.setAttribute("placeholder", "");
-  fade.classList.add("fade");
+  textInputField.setAttribute("placeholder", "");
+  fadeEffect.classList.add("fadeEffect");
   imgContainer.forEach((element) => {
-    element.classList.add("slider");
+    element.classList.add("sliderEffect");
     element.classList.add("imgContainer");
   });
 });
 
-inputFoto.addEventListener("change", function (evento) {
+textInputField.addEventListener("change", function (evento) {
   entrada = String(evento.target.value).toLowerCase();
 
   switch (entrada) {
@@ -28,10 +28,10 @@ inputFoto.addEventListener("change", function (evento) {
     case "1":
     case "cidade":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "slider");
-        fade.classList.remove("fade");
+        element.classList.remove(...classes, "sliderEffect");
+        fadeEffect.classList.remove("fadeEffect");
         element.classList.add(classes[0]);
-        localP.innerHTML = locais[0];
+        tituloCidade.innerHTML = locais[0];
       });
 
       break;
@@ -39,46 +39,46 @@ inputFoto.addEventListener("change", function (evento) {
     case "2":
     case "deck":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "slider");
-        fade.classList.remove("fade");
+        element.classList.remove(...classes, "sliderEffect");
+        fadeEffect.classList.remove("fadeEffect");
         element.classList.add(classes[1]);
-        localP.innerHTML = locais[1];
+        tituloCidade.innerHTML = locais[1];
       });
       break;
     case "03":
     case "3":
     case "floresta":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "slider");
-        fade.classList.remove("fade");
+        element.classList.remove(...classes, "sliderEffect");
+        fadeEffect.classList.remove("fadeEffect");
         element.classList.add(classes[2]);
-        localP.innerHTML = locais[2];
+        tituloCidade.innerHTML = locais[2];
       });
       break;
     case "04":
     case "4":
     case "lago":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "slider");
-        fade.classList.remove("fade");
+        element.classList.remove(...classes, "sliderEffect");
+        fadeEffect.classList.remove("fadeEffect");
         element.classList.add(classes[3]);
-        localP.innerHTML = locais[3];
+        tituloCidade.innerHTML = locais[3];
       });
       break;
     case "05":
     case "5":
     case "montanha":
       imgContainer.forEach((element) => {
-        element.classList.remove(...classes, "slider");
-        fade.classList.remove("fade");
+        element.classList.remove(...classes, "sliderEffect");
+        fadeEffect.classList.remove("fadeEffect");
         element.classList.add(classes[4]);
-        localP.innerHTML = locais[4];
+        tituloCidade.innerHTML = locais[4];
       });
       break;
 
     default:
-      inputFoto.value = "";
-      inputFoto.setAttribute("placeholder", "Não encontrado");
+      textInputField.value = "";
+      textInputField.setAttribute("placeholder", "Não encontrado");
 
       break;
   }
